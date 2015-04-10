9a3083421cb642765c6f96953a897c4ace1ccd78
fbarocci <federico.barocci@gmail.com>
2015-04-10 18:34:53 +0200

    GUI modified and minor changes in PlayerGui + GameView

--- per_un_pugno_di_assi/src/fede/gui/it/unibo/cs/sd/poker/gui/view/GameView.java
+++ per_un_pugno_di_assi/src/fede/gui/it/unibo/cs/sd/poker/gui/view/GameView.java
@@ -1,18 +1,25 @@
 package it.unibo.cs.sd.poker.gui.view;
 
+import it.unibo.cs.sd.poker.game.core.Card;
+import it.unibo.cs.sd.poker.gui.controllers.actionlisteners.Bet;
+import it.unibo.cs.sd.poker.gui.controllers.actionlisteners.Check;
+import it.unibo.cs.sd.poker.gui.controllers.actionlisteners.Fold;
 import it.unibo.cs.sd.poker.gui.view.elements.BackgroundGUI;
 import it.unibo.cs.sd.poker.gui.view.elements.CardGUI;
 import it.unibo.cs.sd.poker.gui.view.elements.ElementGUI;
 import it.unibo.cs.sd.poker.gui.view.elements.PlayerGUI;
 import it.unibo.cs.sd.poker.gui.view.elements.TransparentPanel;
+import it.unibo.cs.sd.poker.gui.view.elements.utils.CardsUtils;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -28,44 +35,117 @@ public class GameView {
 	// TODO usare playerskeeper
 	private Map<String, PlayerGUI> playersGui = new LinkedHashMap<>();
 	
-	public JLabel lblWinners = new JLabel("", SwingConstants.CENTER);
+	public JLabel lblWinners = new JLabel("");	//, SwingConstants.CENTER);
 	public List<CardGUI> tableCardsGui = new ArrayList<>();
+	public CardGUI card1;
+	public CardGUI card2;
+	
+	private final Map<String, JButton> buttons = new HashMap<>();
+	public JLabel lblClientPlayerName = new JLabel("", SwingConstants.CENTER);
+	public JLabel lblClientPlayerScore = new JLabel("");
+	public JLabel lblClientPlayerPot = new JLabel("");
 	
 	public GameView() {
 		super();
 		frame.setLayout(null);
 		frame.setTitle("Poker Distributed Hold'em");
 		frame.setContentPane(background);
-		frame.setBounds(0, 0, 1280, 720);
+		frame.setBounds(0, 0, 1280, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
-	}
-	
-	private void initTableGui() {		
-		for (int i = 0; i < 5; i++) {
-			CardGUI c = new CardGUI(300 + 150*i, 250);
-			tableCardsGui.add(c);
-			addElementGui(c);
-		}
+		
+		JLabel lblTitle = new JLabel("<html><p style='text-align:center; border-bottom:1px solid #000;padding-bottom:10px;'>Poker Distributed Hold'em</p></html>", SwingConstants.CENTER);
+		lblTitle.setForeground(new Color(255, 230, 0));
+		lblTitle.setFont(new Font("SansSerif", Font.BOLD, 22));
+		lblTitle.setBounds(10, 10, 160, 100);
+		frame.getContentPane().add(lblTitle);
 	}
 	
 	public void init(List<Player> players) {
 		initElementsGUI(players);
 		initTableGui();
-		
-		lblWinners.setForeground(new Color(0, 0, 0));
-		lblWinners.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 16));
-		lblWinners.setBounds(400, 50, 500, 50);
-		frame.getContentPane().add(lblWinners);
+		initActionsGui("Player Name", 200, 0); ///*, players.get(0).getCards()*/);
+	}
+	
+	private void initTableGui() {		
+		for (int i = 0; i < 5; i++) {
+			CardGUI c = new CardGUI(375 + 150*i, 40);
+			tableCardsGui.add(c);
+			addElementGui(c);
+		}
 		
 		JPanel panel = new TransparentPanel();
 		panel.setBackground(new Color(174, 234, 255, 70));
 		panel.setBorder(new LineBorder(new Color(255, 255, 255), 1));
-		panel.setBounds(275, 225, 740, 185);
+		panel.setBounds(350, 15, 740, 185);
 		frame.getContentPane().add(panel);
+		
+		lblWinners.setForeground(new Color(255, 230, 0));
+		lblWinners.setFont(new Font("SansSerif", Font.BOLD, 16));
+		lblWinners.setBounds(20, 400, 500, 50);
+		lblWinners.setText("Il vincitore è ...");
+		frame.getContentPane().add(lblWinners);
 	}
 	
+	private void initActionsGui(String clientPlayer, int chips, int score /*, List<Card> cards*/) {
+		lblClientPlayerName.setBounds(0, 130, 180, 50);
+		lblClientPlayerName.setForeground(new Color(0, 0, 0));
+		lblClientPlayerName.setFont(new Font("SansSerif", Font.BOLD, 14));
+		lblClientPlayerName.setText(clientPlayer);
+		frame.getContentPane().add(lblClientPlayerName);
+		
+		lblClientPlayerPot.setBounds(10, 330, 180, 50);
+		lblClientPlayerPot.setForeground(new Color(0, 0, 0));
+		lblClientPlayerPot.setFont(new Font("SansSerif", Font.BOLD, 14));
+		lblClientPlayerPot.setText("Fiches: "+ chips);
+		frame.getContentPane().add(lblClientPlayerPot);
+		
+		lblClientPlayerScore.setBounds(10, 350, 180, 50);
+		lblClientPlayerScore.setForeground(new Color(0, 0, 0));
+		lblClientPlayerScore.setFont(new Font("SansSerif", Font.BOLD, 14));
+		lblClientPlayerScore.setText("Punteggio: " + score);
+		frame.getContentPane().add(lblClientPlayerScore);
+		
+//		card1 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(cards.get(0)), 24, 50);
+//		card2 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(cards.get(1)), 102, 50);
+//		frame.getContentPane().add(card1);
+//		frame.getContentPane().add(card2);
+		
+		for (String	s: new String[]{"Check", "Fold", "Bet"}) {
+			buttons.put(s, new JButton(s));
+		}
+		
+		JButton checkButton = buttons.get("Check");
+		JButton foldButton = buttons.get("Fold");
+		JButton betButton = buttons.get("Bet");
+		
+		checkButton.setBounds(40, 180, 100, 40);
+		foldButton.setBounds(40, 230, 100, 40);
+		betButton.setBounds(40, 280, 100, 40);
+		
+		frame.getContentPane().add(checkButton);
+		frame.getContentPane().add(foldButton);
+		frame.getContentPane().add(betButton);
+		
+		checkButton.addActionListener( new Check(/*model, view*/) );
+		foldButton.addActionListener( new Fold(/*model, view*/) );
+		betButton.addActionListener( new Bet(/*model, view*/) );
+		
+		JPanel panel = new TransparentPanel();
+		panel.setBackground(new Color(0, 104, 139, 70));
+//		panel.setBackground(new Color(188, 221, 17, 255));
+		//panel.setBorder(new LineBorder(new Color(65, 146, 75), 1));
+		panel.setBounds(0, 0, 180, 400);
+		frame.getContentPane().add(panel);
+		
+		JPanel panel2 = new TransparentPanel();
+		panel2.setBackground(new Color(176, 23, 31, 255));
+		//panel2.setBorder(new LineBorder(new Color(65, 146, 75), 1));
+		panel2.setBounds(0, 400, 1280, 50);
+		frame.getContentPane().add(panel2);
+	}
+
 	/* TODO Per la costruzione della gui di welcome
 	public void createIntroGUI() {
 		acceptButton.setBounds(12, 12, 103, 25);
@@ -133,8 +213,8 @@ public class GameView {
 		PlayerGUI playerGui = new PlayerGUI(p.getName(),  
 				p.getCards().get(0), 
 				p.getCards().get(1), 
-				(180 * playersGui.size()) + 110, 
-				500);
+				(135 * playersGui.size()) + 195, 
+				230);
 		
 		playersGui.put(p.getName(), playerGui);
 		playerGui.draw(frame);
@@ -148,6 +228,7 @@ public class GameView {
 		
 		playersGui.clear();
 		lblWinners.setText("");
+		buttons.values().stream().forEach(b->frame.getContentPane().remove(b));
 	}
 	
 	private void setToken(Integer i) {
--- per_un_pugno_di_assi/src/fede/gui/it/unibo/cs/sd/poker/gui/view/elements/PlayerGUI.java
+++ per_un_pugno_di_assi/src/fede/gui/it/unibo/cs/sd/poker/gui/view/elements/PlayerGUI.java
@@ -1,15 +1,11 @@
 package it.unibo.cs.sd.poker.gui.view.elements;
 
 import it.unibo.cs.sd.poker.game.core.Card;
-import it.unibo.cs.sd.poker.gui.controllers.actionlisteners.Check;
 import it.unibo.cs.sd.poker.gui.view.elements.utils.CardsUtils;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,81 +14,40 @@ import javax.swing.border.LineBorder;
 
 public class PlayerGUI {
 	
-	private Boolean interactive;
 	private Integer x;
 	private Integer y;
 	
 	private CardGUI card1;
 	private CardGUI card2;
 	private final JLabel name = new JLabel("", SwingConstants.CENTER);
-	private final Map<String, JButton> buttons = new HashMap<>();
+	private final JLabel action = new JLabel("ACTION", SwingConstants.CENTER);
+	private final JLabel score = new JLabel("PUNTI: 0", SwingConstants.CENTER);
 	private final JPanel box = new TransparentPanel();
 	
-	private final static int xOffset = 60;
-	
 	public PlayerGUI(String playerId, Card card0, Card card1, Integer x, Integer y) {
-		this.interactive = true;
 		this.x = x;
 		this.y = y;
 		
-		this.card1 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(card0), x, y);
-		this.card2 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(card1), x + xOffset, y);
+		this.card1 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(card0), x + 5, y);
+		this.card2 = new CardGUI(CardsUtils.INSTANCE_SMALL.getImageCard(card1), x + 62, y);
 		
 		this.name.setText(playerId);
 		
-		this.box.setBackground(new Color(188, 221, 17, 70));
-		this.box.setBorder(new LineBorder(new Color(65, 146, 75), 1));
+		//this.box.setBackground(new Color(255, 230, 0, 200));
+		this.box.setBackground(new Color(255, 255, 255, 60));
+		this.box.setBorder(new LineBorder(new Color(255, 230, 0, 255), 1));
 		//this.box.setBounds(this.x-10, this.y-10, 135, 170);
-		this.box.setBounds(this.x-10, this.y+25, 135, 135);
-		
-		populateButtonsMap();
+		this.box.setBounds(this.x, this.y+25, 120, 135);
 	}
-	
-	private void populateButtonsMap() {
-		for (String	s: new String[]{"Check", "Call"}) {
-			buttons.put(s, new JButton(s));
-		}
-	}
-	
-	/*public PlayerGUI(Player player, Boolean yesIsInteractive, Integer x, Integer y) {
-		this.interactive = yesIsInteractive;
-		this.x = x;
-		this.y = y;
-		
-		if (interactive) {
-			this.card1 = new CardGUI(player.getCards().get(0), x, y, cardDefaultWidth , cardDefaultHeight);
-			this.card2 = new CardGUI(player.getCards().get(1), x + xOffset, y, cardDefaultWidth , cardDefaultHeight );
-		}
-		
-//		else {
-//			this.card1 = new CardGUI(this.x, this.y, cardDefaultWidth, cardDefaultHeight);
-//			this.card2 = new CardGUI(this.x + xOffset, this.y, cardDefaultWidth, cardDefaultHeight);
-//		}
-		
-		this.name.setText(player.getName());
-		
-		this.box.setBackground(new Color(188, 221, 17, 70));
-		this.box.setBorder(new LineBorder(new Color(65, 146, 75), 1));
-		//this.box.setBounds(this.x-10, this.y-10, 135, 170);
-		this.box.setBounds(this.x-10, this.y+25, 135, 135);
-	}*/
 	
 	public CardGUI getCard1() {
 		return card1;
 	}
 	
-//	public void setCard1(Card card) {
-//		card1.setCard(card);
-//	}
-	
 	public CardGUI getCard2() {
 		return card2;
 	}
 	
-//	public void setCard2(Card card) {
-//		card2.setCard(card);
-//	}
-
 	public String getName() {
 		return name.getText();
 	}
@@ -102,40 +57,30 @@ public class PlayerGUI {
 	}
 
 	public void draw(JFrame frame) {
-		JButton checkButton = buttons.get("Check");
-		
-		if (this.interactive)
-			checkButton.setBounds(this.x+17, this.y + 120, 80, 30);
-		
-		name.setBounds(this.x, this.y + 80, 115, 40);
+		name.setBounds(this.x, this.y + 90, 115, 20);
 		name.setForeground(new Color(0, 0, 0));
-		name.setFont(new Font("SansSerif", Font.BOLD, 16));
+		name.setFont(new Font("SansSerif", Font.BOLD, 15));
+		
+		action.setBounds(this.x, this.y + 120, 115, 20);
+		action.setForeground(new Color(0, 0, 0));
+		action.setFont(new Font("SansSerif", Font.BOLD, 11));
+		
+		score.setBounds(this.x, this.y + 135, 115, 20);
+		score.setForeground(new Color(0, 0, 0));
+		score.setFont(new Font("SansSerif", Font.BOLD, 11));
 		
 		frame.getContentPane().add(card1);
 		frame.getContentPane().add(card2);
 		frame.getContentPane().add(name);
-		
-		if (this.interactive) {
-			frame.getContentPane().add(checkButton);
-			checkButton.addActionListener( new Check(/*model, view*/) );
-		}
-		
+		frame.getContentPane().add(action);
+		frame.getContentPane().add(score);
 		frame.getContentPane().add(box);
 	}
 	
-	/*@Override
-	public String toString() {
-		return name.getText() + " " + card1.getCardImgPath() + " "
-				+ card2.getCardImgPath() + " " + x + " " + y;
-	}*/
-
 	public void clearFromGui(JFrame frame) {
-		// TODO Auto-generated method stub
 		frame.getContentPane().remove( card1 );
 		frame.getContentPane().remove( card2 );
 		frame.getContentPane().remove( name );
-//		playerGui.clearButtonsMap(frame);
-		buttons.values().stream().forEach(b->frame.getContentPane().remove(b));
 		frame.getContentPane().remove( box );
 	}
 }
